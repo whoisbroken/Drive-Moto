8a657487a0d441bc2d54c971d9126cc0efa13084
Alexander Petriga <cazhqa666@gmail.com>
2020-09-20 23:16:30 +0300

    add hero section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+$(function () {
+  $('.hero__banner-slider').slick({
+    dots: true,
+    prevArrow: '<button class="hero__banner-slider__btn hero__banner-slider__btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="hero__banner-slider__btn hero__banner-slider__btnnext"><img src="images/arrow-right.svg" alt=""></button>'
+  })
 });
