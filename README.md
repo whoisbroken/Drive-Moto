6409b8263051f7ada905eaf65b07a52b117805d8
Alexander Petriga <cazhqa666@gmail.com>
2020-09-28 20:00:54 +0300

    add search section/update readme file

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 # Used technologies
 <li>HTML5</li>
 <li>CSS3</li>
+<li>normalize</li>
+<li>jQuery</li>
+<li>slick</li>
 
 # Build on GitHub Pages
 <https://whoisbroken.github.io/Drive-Moto>
--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ $(function () {
     prevArrow: '<button class="hero__banner-slider__btn hero__banner-slider__btnprev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="hero__banner-slider__btn hero__banner-slider__btnnext"><img src="images/arrow-right.svg" alt=""></button>'
   })
+
+  $('.search__tabs-item').on('click', function(e) {
+    e.preventDefault();
+
+    $('.search__tabs-item').removeClass('search__tabs-item--active');
+    $('.search__content-item').removeClass('search__content-item--active');
+
+    $(this).addClass('search__tabs-item--active');
+    $($(this).attr('href')).addClass('search__content-item--active');
+
+  });
+
+
 });
